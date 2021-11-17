775170744137f1dffc498e76b93302e8c89bacb6
Michael McLoughlin <80280306+Lundi19@users.noreply.github.com>
2021-11-17 12:01:38 +0000

    react router functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Game from './components/Game'
+import Footer from './components/Footer'
+import Modal from './components/Modal'
+import Play from './components/Play'
 
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-      </>
-    );
-  }
+const App = () => {
+  return (
+  
+    <div className="container">
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
